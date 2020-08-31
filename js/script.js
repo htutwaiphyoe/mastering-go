9df41e953f9690af497d2cf98e4cc32493fdb135
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-08-31 19:20:14 +0630

    Final Release 3

--- js/script.js
+++ js/script.js
@@ -159,7 +159,9 @@ DOMs.box.addEventListener("change", (e) => {
     const value = e.target.value;
     const changeSelectBox = (player, selectBox) => {
         DOMs[selectBox].classList.remove(color[player]);
-        DOMs.headerText.classList.remove(color[player]);
+        if (color.player1 !== color.player2) {
+            DOMs.headerText.classList.remove(color[player]);
+        }
 
         color[player] = value;
         DOMs[selectBox].classList.add(color[player]);
