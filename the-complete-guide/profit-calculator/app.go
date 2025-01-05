a3296c30df2c246698df650fd2a03d37d1afc83d
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-01-05 17:14:10 +0700

    feat(profit-calculator): module summary

--- the-complete-guide/profit-calculator/app.go
+++ the-complete-guide/profit-calculator/app.go
@@ -1,24 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getInput("Enter the revenue: ")
-	expenses := getInput("Enter the expenses: ")
-	taxPercent := getInput("Enter the tax rate: ")
+	revenue, err := getInput("Enter the revenue: ")
+
+	if err != nil {
+		fmt.Println("Revenue must not be less than zero")
+		return
+	}
+
+	expenses, err := getInput("Enter the expenses: ")
+
+	if err != nil {
+		fmt.Println("Expense must not be less than zero")
+		return
+	}
+
+	taxPercent, err := getInput("Enter the tax rate: ")
+
+	if err != nil {
+		fmt.Println("Tax rate must not be less than zero")
+		return
+	}
 
 	earningsBeforeTax, earningsAfterTax := calculateEarnings(revenue, expenses, taxPercent)
+
+	os.WriteFile("profile.txt", []byte(fmt.Sprintf("Ebt: %.2f\nEAT: %.2f\n", earningsBeforeTax, earningsAfterTax)), 0644)
 
 	ebt := fmt.Sprintf("Earnings before tax: %.2f\n", earningsBeforeTax)
 	eat := fmt.Sprintf("Earnings after tax: %.2f\n", earningsAfterTax)
 
 	fmt.Println(ebt, eat)
+
 }
 
-func getInput(prompt string) (value float64) {
+func getInput(prompt string) (value float64, err error) {
 	fmt.Print(prompt)
 	fmt.Scan(&value)
-	return value
+
+	if value <= 0 {
+		return 0, errors.New("invalid value")
+	}
+	return value, err
 }
 
 func calculateEarnings(revenue, expenses, taxPercent float64) (earningsBeforeTax float64, earningsAfterTax float64) {
