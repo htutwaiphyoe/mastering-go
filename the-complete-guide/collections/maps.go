60b38706f0fb1c6d2fcee4e6b1efadb2dd0a1fda
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-03-29 12:17:12 +0700

    feat(the-complete-guide): make with map

--- the-complete-guide/collections/maps.go
+++ the-complete-guide/collections/maps.go
@@ -20,4 +20,19 @@ func maps() {
 
 	websites["x"] = "www.x.com"
 	fmt.Println(websites)
+
+	slices := make([]string, 0, 5)
+
+	slices = append(slices, "a")
+	slices = append(slices, "b")
+	slices = append(slices, "c")
+
+	fmt.Println(slices)
+
+	maps := make(map[string][]string, 5)
+	maps["a"] = []string{"a", "b", "c"}
+	maps["b"] = []string{"d", "e", "f"}
+	maps["c"] = []string{"g", "h", "i"}
+
+	fmt.Println(maps)
 }
