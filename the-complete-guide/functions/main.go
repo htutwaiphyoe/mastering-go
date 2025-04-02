6330956c2f5d1364e70c343ab3bbdbbf22df5f67
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-04-02 13:22:22 +0700

    feat(the-complete-guide): first class functions

--- the-complete-guide/functions/main.go
+++ the-complete-guide/functions/main.go
@@ -1,42 +1,6 @@
 package main
 
-import "fmt"
-
-type fn func(float64) float64
-
 func main() {
+	firstClass()
 
-	number := []float64{1, 2.5, 3.0, 4}
-
-	doubled := transform(&number, multiply(2))
-
-	fmt.Println("Doubled:", doubled)
-
-	tripled := transform(&number, multiply(3))
-
-	fmt.Println("Tripled:", tripled)
-
-	quadrupled := transform(&number, multiply(4))
-
-	fmt.Println("Quadrupled:", quadrupled)
-
-}
-
-func transform(numbers *[]float64, fn fn) []float64 {
-	result := make([]float64, len(*numbers))
-
-	for i, v := range *numbers {
-		result[i] = fn(v)
-	}
-
-	return result
-}
-
-func multiply(by float64) fn {
-
-	fn := func(x float64) float64 {
-		return x * by
-	}
-
-	return fn
 }
