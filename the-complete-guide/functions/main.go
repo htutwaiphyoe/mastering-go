1e5e6c45c4784251556e5ae44aaad426e7d0bc79
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-04-03 13:13:39 +0700

    feat(the-complete-guide): function as return

--- the-complete-guide/functions/main.go
+++ the-complete-guide/functions/main.go
@@ -2,21 +2,27 @@ package main
 
 import "fmt"
 
+type fn func(float64) float64
+
 func main() {
 
 	number := []float64{1, 2.5, 3.0, 4}
 
-	doubled := transform(&number, double)
+	doubled := transform(&number, multiply(2))
 
 	fmt.Println("Doubled:", doubled)
 
-	tripled := transform(&number, triple)
+	tripled := transform(&number, multiply(3))
 
 	fmt.Println("Tripled:", tripled)
 
+	quadrupled := transform(&number, multiply(4))
+
+	fmt.Println("Quadrupled:", quadrupled)
+
 }
 
-func transform(numbers *[]float64, fn func(float64) float64) []float64 {
+func transform(numbers *[]float64, fn fn) []float64 {
 	result := make([]float64, len(*numbers))
 
 	for i, v := range *numbers {
@@ -32,4 +38,17 @@ func double[T int | float64](x T) T {
 
 func triple[T int | float64](x T) T {
 	return x * 3
+}
+
+func multiply(by int) fn {
+	if by == 2 {
+		return double
+	}
+	if by == 3 {
+		return triple
+	}
+
+	return func(x float64) float64 {
+		return x * float64(by)
+	}
 }
