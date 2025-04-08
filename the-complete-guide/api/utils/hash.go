5f3c9a74679af11e5cf551234650138f4f1c1866
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-04-09 00:21:51 +0700

    feat(the-complete-guide): bcrypt

---
+++ the-complete-guide/api/utils/hash.go
@@ -0,0 +1,9 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func Hash(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(hashed), err
+}
